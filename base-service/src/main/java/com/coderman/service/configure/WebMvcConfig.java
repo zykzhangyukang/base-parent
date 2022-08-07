180f9907109d0263cb438bc80eefaa48238cc1c4
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-08-07 19:53:35 +0800

    添加xss过滤，添加系统启动关闭 hook

---
+++ base-service/src/main/java/com/coderman/service/configure/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package com.coderman.service.configure;
+
+import com.coderman.service.xss.XssFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author coderman
+ * @date 2022/8/7 19:40
+ */
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@SuppressWarnings("all")
+public class WebMvcConfig {
+
+    /**
+     * XssFilter Bean
+     */
+    @Bean
+    public FilterRegistrationBean<XssFilter> xssFilterRegistrationBean() {
+        FilterRegistrationBean<XssFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.setEnabled(true);
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> initParameters = new HashMap<>(3);
+        initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+        initParameters.put("isIncludeRichText", "true");
+        filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+}

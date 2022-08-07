88704506fb95bf68d28f947b587baae531a0cf70
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-08-07 11:35:09 +0800

    代码优化

---
+++ base-service/src/main/java/com/coderman/service/aspect/AuthAspect.java
@@ -0,0 +1,24 @@
+package com.coderman.service.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 权限拦截器
+ *
+ * @author coderman
+ * @date 2022/8/7 11:25
+ */
+@Aspect
+@Component
+@Slf4j
+@Order(value = 2300)
+public class AuthAspect {
+
+
+
+
+
+}

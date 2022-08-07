a9073657a67d823c3ca9cd52e1804449cb2eb8cb
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-08-07 11:35:09 +0800

    代码优化

--- base-auth/base-auth-service/src/main/java/com/coderman/auth/dao/user/UserDAO.java
+++ base-auth/base-auth-service/src/main/java/com/coderman/auth/dao/user/UserDAO.java
@@ -2,10 +2,21 @@ package com.coderman.auth.dao.user;
 
 import com.coderman.auth.model.user.UserExample;
 import com.coderman.auth.model.user.UserModel;
+import com.coderman.auth.vo.user.UserVO;
 import com.coderman.mybatis.dao.BaseDAO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserDAO extends BaseDAO<UserModel, UserExample> {
 
 
 
+    /**
+     * 用户列表
+     *
+     * @param queryVO
+     * @return
+     */
+    List<UserVO> page(@Param(value = "queryVO") UserVO queryVO);
 }

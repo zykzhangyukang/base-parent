a4b9bf21b3f9bee210d3458a92f9eb982c187132
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-08-26 20:52:26 +0800

    优化代码

---
+++ base-api/src/main/java/com/coderman/api/util/PageUtil.java
@@ -0,0 +1,47 @@
+package com.coderman.api.util;
+
+
+import com.coderman.api.vo.PageVO;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class PageUtil {
+
+
+    /**
+     * 分页参数
+     *
+     * @param conditionMap map
+     * @param currentPage  当前页面
+     * @param pageSize     每页显示
+     * @return
+     */
+    public static Map<String, Object> getConditionMap(Map<String, Object> conditionMap, Integer currentPage, Integer pageSize) {
+
+        Assert.notNull(conditionMap, "conditionMap is null");
+        Assert.notNull(currentPage, "currentPage is null");
+        Assert.notNull(pageSize, "pageSize is null");
+
+        Integer offset = currentPage < 1 ? 1 : (currentPage - 1) * pageSize;
+
+        conditionMap.put("offSet", offset);
+        conditionMap.put("size", pageSize);
+        return conditionMap;
+    }
+
+
+    /**
+     * 获取分页结果
+     * @param count
+     * @param dataList
+     * @param currentPage
+     * @param pageSize
+     * @param <T>
+     * @return
+     */
+    public static <T> PageVO<List<T>> getPageVO(Long count, List<T> dataList, Integer currentPage, Integer pageSize) {
+        return new PageVO<>(count, dataList, currentPage, pageSize);
+    }
+}

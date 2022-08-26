6265dae09868bf44ce3a62ace1c33f6b6d81af45
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-08-26 22:18:29 +0800

    通用redis集成

---
+++ base-api/src/main/java/com/coderman/api/constant/RedisDbConstant.java
@@ -0,0 +1,14 @@
+package com.coderman.api.constant;
+
+public interface RedisDbConstant {
+
+    /**
+     * 默认0号DB
+     */
+    int REDIS_DB_DEFAULT = 0;
+
+    /**
+     * 权限认证相关DB
+     */
+    int REDIS_DB_AUTH = 1;
+}

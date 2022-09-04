e23bcf7f2cd9a0d29e35d60dad81a42b61b8d506
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-09-04 19:45:21 +0800

    权限系统代码优化

--- base-auth/base-auth-web/src/main/java/com/coderman/auth/controller/AuthController.java
+++ base-auth/base-auth-web/src/main/java/com/coderman/auth/controller/AuthController.java
@@ -1,14 +1,25 @@
 package com.coderman.auth.controller;
 
+import com.coderman.api.constant.CommonConstant;
 import com.coderman.api.constant.RedisDbConstant;
 import com.coderman.api.constant.ResultConstant;
+import com.coderman.api.exception.BusinessException;
 import com.coderman.api.util.ResultUtil;
+import com.coderman.api.vo.AuthUserVO;
 import com.coderman.api.vo.ResultVO;
+import com.coderman.auth.service.user.UserService;
+import com.coderman.auth.vo.user.UserInfoVO;
 import com.coderman.service.api.RescApi;
+import com.coderman.service.config.PropertyConfig;
 import com.coderman.service.dict.ConstItem;
 import com.coderman.service.dict.ConstItems;
 import com.coderman.service.redis.RedisService;
+import com.coderman.swagger.annotation.ApiReturnParam;
+import com.coderman.swagger.annotation.ApiReturnParams;
+import com.coderman.swagger.constant.SwaggerConstant;
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -35,15 +46,47 @@ public class AuthController {
     private RescApi rescApi;
 
 
+    @Autowired
+    private UserService userService;
+
+
     /**
      * 获取权限系统所有资源信息
      *
      * @param project 项目名称
      * @return
      */
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_POST, value = "获取权限系统所有资源信息api")
     @PostMapping(value = "/api/resc/all")
-    public ResultVO< Map<String, Set<Integer>>> sysRescAll(@RequestParam(value = "domain") String project){
-        return this.rescApi.getSystemAllRescMap(project);
+    public ResultVO<Map<String, Set<Integer>>> sysRescAll(@RequestParam(value = "domain") String project,
+                                                          @RequestParam(value = "securityCode", required = false) String securityCode) {
+
+        if (StringUtils.isBlank(securityCode) || !StringUtils.equals(securityCode, PropertyConfig.getConfigValue("auth-security-code"))) {
+
+            return ResultUtil.getFail("非法访问,securityCode错误");
+
+        }else {
+            return this.rescApi.getSystemAllRescMap(project);
+        }
+
+    }
+
+
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_POST, value = "获取用户信息api")
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+            @ApiReturnParam(name = "AuthUserVO", value = {"realName", "rescIdList", "deptCode", "username", "token"}),
+    })
+    @PostMapping(value = "/api/user/info")
+    public ResultVO<AuthUserVO> info(@RequestHeader(value = CommonConstant.USER_TOKEN_NAME, required = false) String token,
+                                     @RequestHeader(value = "securityCode", required = false) String securityCode) {
+
+        if (StringUtils.isBlank(securityCode) || !StringUtils.equals(securityCode, PropertyConfig.getConfigValue("auth-security-code"))) {
+
+            return ResultUtil.getFail("非法访问,securityCode错误");
+        } else {
+            return this.userService.getUserByToken(token);
+        }
     }
 
 
@@ -53,9 +96,9 @@ public class AuthController {
      * @return
      */
     @GetMapping(value = "/const/all")
-    public ResultVO<Map<String,List<ConstItems>>> constAll() {
+    public ResultVO<Map<String, List<ConstItems>>> constAll() {
 
-        Map<String,List<ConstItems>> resultMap = new HashMap<>();
+        Map<String, List<ConstItems>> resultMap = new HashMap<>();
 
 
         this.redisService.getRedisTemplate().execute(new RedisCallback() {
@@ -76,7 +119,7 @@ public class AuthController {
                     String project = new String(field);
                     List<ConstItems> constItems = (List<ConstItems>) s2.deserialize(connection.hGet(hashKey, field));
 
-                    resultMap.put(project,constItems);
+                    resultMap.put(project, constItems);
                 }
 
                 return null;
@@ -84,7 +127,7 @@ public class AuthController {
         });
 
 
-        ResultVO<Map<String,List<ConstItems>>> resultVO = new ResultVO<>();
+        ResultVO<Map<String, List<ConstItems>>> resultVO = new ResultVO<>();
         resultVO.setCode(ResultConstant.RESULT_CODE_200);
         resultVO.setResult(resultMap);
         return resultVO;

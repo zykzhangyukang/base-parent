edf1da4e9eeb38dae548569969c938f847ebc1b4
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-09-25 11:02:52 +0800

    均摊业务抽象

---
+++ base-business/base-business-share/src/main/java/com/coderman/business/request/ShareParamVO.java
@@ -0,0 +1,23 @@
+package com.coderman.business.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class ShareParamVO {
+
+    @ApiModelProperty(value = "待分摊项")
+    private BigDecimal waitShare;
+
+    @ApiModelProperty(value = "待分摊分母")
+    private BigDecimal waitShareDenominator;
+
+    @ApiModelProperty(value = "要分摊总数的明细集合")
+    private List<ShareParamDetailVO> shareParamDetailList;
+
+    @ApiModelProperty(value = "待分摊项是否需要全部摊在明细上")
+    private Boolean isShareAll;
+}

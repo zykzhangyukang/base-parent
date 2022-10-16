e02f5b751e92b564547bf30f2a21784d4be245c4
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-10-16 22:47:16 +0800

    同步系统服务端消费者

--- base-sync-server/base-sync-service/src/main/java/com/coderman/sync/constant/SyncConstant.java
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/constant/SyncConstant.java
@@ -4,4 +4,10 @@ public interface SyncConstant {
 
     // 数据库类型 mysql
     public static final String DB_TYPE_MYSQL = "mysql";
+
+    /**
+     * 消息类型: 回调,同步
+     */
+    String MSG_TYPE_SYNC = "sync";
+    String MSG_TYPE_CALLBACK = "callback";
 }
--- base-sync-server/base-sync-service/src/main/java/com/coderman/sync/task/SyncTask.java
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/task/SyncTask.java
@@ -1,15 +1,61 @@
 package com.coderman.sync.task;
 
+import com.coderman.sync.plan.meta.MsgMeta;
+import com.coderman.sync.plan.meta.PlanMeta;
+import com.coderman.sync.result.ResultModel;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Date;
+
 /**
  * 同步任务
- *
  */
 @Slf4j
 @Data
 public class SyncTask {
 
+    // 同步计划对象
+    private PlanMeta planMeta;
 
+    // 封装消息对象
+    private MsgMeta msgMeta;
+
+    // 封装同步结果
+    private ResultModel resultModel;
+
+    // 同步时间
+    private Date syncTime;
+
+    /**
+     * 构建同步任务
+     *
+     * @param msg
+     * @param mqId
+     * @param retryTimes
+     * @return
+     */
+    public static SyncTask build(String msg, String mqId, int retryTimes) {
+
+        SyncTask syncTask = new SyncTask();
+
+        // 解析构建消息对象
+        MsgMeta msgMeta = MsgMeta.build(msg);
+
+        // 构建同步结果记录
+        ResultModel resultModel = new ResultModel();
+
+
+        return null;
+    }
+
+
+    /**
+     * 同步数据
+     *
+     * @return
+     */
+    public String sync() {
+        return null;
+    }
 }

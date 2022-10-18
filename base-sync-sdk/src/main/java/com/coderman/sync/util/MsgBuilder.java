9a8c9545a2494ba7f496ab9692c98142805f6a9f
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-10-18 21:27:14 +0800

    同步系统完善2

--- base-sync-sdk/src/main/java/com/coderman/sync/util/MsgBuilder.java
+++ base-sync-sdk/src/main/java/com/coderman/sync/util/MsgBuilder.java
@@ -4,10 +4,7 @@ import com.coderman.sync.vo.MsgItem;
 import com.coderman.sync.vo.PlanMsg;
 import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MsgBuilder {
 
@@ -106,6 +103,47 @@ public class MsgBuilder {
         return this;
     }
 
+    public MsgBuilder addWithMustAffectRows(String code,String unique,Integer mustAffectRows){
+
+        ArrayList<String> uniques = new ArrayList<>(1);
+        uniques.add(unique);
+        return this.addWithMustAffectRows(code,uniques,mustAffectRows);
+    }
+
+    public MsgBuilder addWithMustAffectRows(String code,Number unique,Integer mustAffectRows){
+
+        ArrayList<Number> uniques = new ArrayList<>(1);
+        uniques.add(unique);
+        return this.addWithMustAffectRows(code,uniques,mustAffectRows);
+    }
+
+
+
+    @SuppressWarnings("all")
+    private MsgBuilder addWithMustAffectRows(String code, Collection uniques,Integer mustAffectRows){
+
+        if(CollectionUtils.isEmpty(uniques)){
+
+            throw new RuntimeException("同步计划unique不允许为空");
+        }
+
+        if(!this.dataMap.containsKey(code)){
+
+            this.dataMap.put(code,new MsgItem(code,mustAffectRows));
+        }
+
+        for (Object unique : uniques) {
+
+            if(!(unique instanceof String) && !(unique instanceof Integer) && !(unique instanceof Long)){
+
+                throw new RuntimeException("unique数据类型错误");
+            }
+
+            this.dataMap.get(code).getUnique().add(unique.toString());
+        }
+
+        return this;
+    }
 
 
     public PlanMsg build(){

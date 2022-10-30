87868645f1c87a532b7b8abac308d6a0931bec66
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-10-30 19:21:20 +0800

    rocketmq 消息重试定时器使用xxljob

---
+++ base-rocketmq/src/main/java/com/coderman/rocketmq/jobhandler/ResentJobHandler.java
@@ -0,0 +1,30 @@
+package com.coderman.rocketmq.jobhandler;
+
+import com.coderman.rocketmq.service.RocketMqService;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * 失败消息重试
+ */
+@JobHandler(value="resentJobHandler")
+@Component
+@Slf4j
+public class ResentJobHandler extends IJobHandler {
+
+    @Resource
+    private RocketMqService rocketMqService;
+
+    @Override
+    public ReturnT<String> execute(String param) throws Exception {
+
+        rocketMqService.resendMsg();
+
+        return ReturnT.SUCCESS;
+    }
+}

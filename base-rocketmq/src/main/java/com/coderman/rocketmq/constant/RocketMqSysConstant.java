c04984da737b573d2cb7be570f1d8764ed2ea484
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2022-11-01 23:37:21 +0800

    RocketMqService 封装

---
+++ base-rocketmq/src/main/java/com/coderman/rocketmq/constant/RocketMqSysConstant.java
@@ -0,0 +1,18 @@
+package com.coderman.rocketmq.constant;
+
+/**
+ * rocketmq 常量
+ */
+public interface RocketMqSysConstant {
+
+
+    /**
+     * 分割符
+     */
+    String DELIMITER = ":";
+
+    /**
+     *
+     */
+    String TRACE_ID = "trace_id";
+}

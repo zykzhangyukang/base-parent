0440f7b38fa074f97e59e9cbe4545478953d9403
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-01-12 22:32:37 +0800

    同步系统代码完善+1

--- base-sync-server/base-sync-service/src/main/java/com/coderman/sync/task/support/SyncDataTask.java
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/task/support/SyncDataTask.java
@@ -1,10 +1,14 @@
 package com.coderman.sync.task.support;
 
+import com.coderman.service.util.UUIDUtils;
+import com.coderman.sync.callback.meta.CallbackTask;
+import com.coderman.sync.constant.PlanConstant;
 import com.coderman.sync.constant.SyncConstant;
 import com.coderman.sync.context.CallbackContext;
 import com.coderman.sync.context.SyncContext;
 import com.coderman.sync.exception.SyncException;
 import com.coderman.sync.executor.AbstractExecutor;
+import com.coderman.sync.plan.meta.CallbackMeta;
 import com.coderman.sync.plan.meta.ColumnMeta;
 import com.coderman.sync.plan.meta.MsgTableMeta;
 import com.coderman.sync.plan.meta.TableMeta;
@@ -35,6 +39,36 @@ public class SyncDataTask extends AbstractTask {
 
     }
 
+
+    @Override
+    public TaskResult process() {
+
+
+        TaskResult taskResult = super.process();
+
+        // 数据同步成功吧回调加入队列中
+        if (SyncConstant.TASK_CODE_SUCCESS.equals(taskResult.getCode())) {
+
+            if (CollectionUtils.isNotEmpty(taskResult.getResultList())) {
+
+                for (SqlMeta sqlMeta : taskResult.getResultList()) {
+
+                    if (CollectionUtils.isNotEmpty(sqlMeta.getCallbackTaskList())) {
+
+                        CallbackContext.getCallbackContext().addTask(sqlMeta.getCallbackTaskList());
+                    }
+                }
+
+            }
+
+             super.getSyncTask().getResultModel().setSyncContent("同步内容todo");
+        }
+
+        return taskResult;
+    }
+
+
+
     public static SyncDataTask build(SyncTask syncTask, TaskResult taskResult) {
 
         SyncDataTask syncDataTask = new SyncDataTask(syncTask);
@@ -58,101 +92,174 @@ public class SyncDataTask extends AbstractTask {
 
 
         Map<String, SqlMeta> resultMetaMap = new HashMap<>();
+
         if (null != taskResult && null != taskResult.getResultList()) {
 
             for (SqlMeta sqlMeta : taskResult.getResultList()) {
 
                 resultMetaMap.put(sqlMeta.getTableCode(), sqlMeta);
             }
-
-
-            for (MsgTableMeta msgTableMeta : syncTask.getMsgMeta().getTableMetaList()) {
-
-
-                TableMeta tableMeta = SyncContext.getContext().getTableMeta(syncTask.getPlanMeta().getCode(), msgTableMeta.getCode());
-
-                // 封装对象和参数集合
-                SqlMeta sqlMeta = new SqlMeta();
-                sqlMeta.setTableCode(tableMeta.getCode());
-
-                if (SyncConstant.OPERATE_TYPE_INSERT.equals(tableMeta.getType())) {
-
-
-                    // 插入操作
-                    InsertBuilder insertBuilder = InsertBuilder.create(dbType);
-                    insertBuilder.table(tableMeta.getDest());
-
-                    int[] paramTypeList = new int[tableMeta.getColumnMetas().size()];
-
-                    int i = 0;
-
-                    for (ColumnMeta columnMeta : tableMeta.getColumnMetas()) {
-
-                        insertBuilder.column(columnMeta.getDest());
-                        paramTypeList[i] = JdbcUtils.TYPE_UNKNOWN;
-                        i++;
-                    }
-
-                    insertBuilder.column("sync_time");
-                    List<Object[]> paramList = this.generateParam(resultMetaMap, msgTableMeta, tableMeta, syncTask.getSyncTime(), true);
-
-                    insertBuilder.groupCount(paramList.size());
-                    sqlMeta.setParamList(paramList);
-                    sqlMeta.setSql(insertBuilder.sql());
-                    sqlMeta.setArgTypes(paramTypeList);
-                    sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_INSERT);
-                }
-
-                if (SyncConstant.OPERATE_TYPE_DELETE.equals(tableMeta.getType())) {
-
-                    DeleteBuilder deleteBuilder = DeleteBuilder.create(dbType);
-                    deleteBuilder.table(tableMeta.getDest());
-                    deleteBuilder.whereIn(tableMeta.getUnique().getValue(), msgTableMeta.getUniqueList().size());
-                    deleteBuilder.groupCount(1);
-
-                    sqlMeta.setSql(deleteBuilder.sql());
-                    sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_DELETE);
-
-                    String targetType = msgTableMeta.getUniqueType();
-
-                    if ("_id".equals(tableMeta.getUnique().getValue())) {
-                        targetType = SyncConvert.DATA_TYPE_OBJECTID;
-                    }
-
-                    sqlMeta.setParamList(SyncConvert.toArrayList(SyncConvert.convert(msgTableMeta.getUniqueList(), targetType)));
-                }
-
-                if (SyncConstant.OPERATE_TYPE_UPDATE.equals(tableMeta.getType())) {
-
-                    UpdateBuilder updateBuilder = UpdateBuilder.create(dbType);
-                    updateBuilder.table(tableMeta.getDest());
-
-                    int[] paramTypeList = new int[tableMeta.getColumnMetas().size()];
-                    int i = 0;
-
-                    for (ColumnMeta columnMeta : tableMeta.getColumnMetas()) {
-
-                        updateBuilder.column(columnMeta.getDest());
-                        paramTypeList[i] = JdbcUtils.TYPE_UNKNOWN;
-                        i++;
-                    }
-
-                    updateBuilder.column("sync_time");
-                    updateBuilder.whereIn(tableMeta.getUnique().getValue(), 1);
-
-                    List<Object[]> paramList = this.generateParam(resultMetaMap, msgTableMeta, tableMeta, syncTask.getSyncTime(), false);
-                    updateBuilder.groupCount(paramList.size());
-
-                    sqlMeta.setParamList(paramList);
-                    sqlMeta.setSql(updateBuilder.sql());
-                    sqlMeta.setArgTypes(paramTypeList);
-                    sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_UPDATE);
-                }
-
-                executor.sql(sqlMeta);
-            }
         }
 
+
+        for (MsgTableMeta msgTableMeta : syncTask.getMsgMeta().getTableMetaList()) {
+
+
+            TableMeta tableMeta = SyncContext.getContext().getTableMeta(syncTask.getPlanMeta().getCode(), msgTableMeta.getCode());
+
+            // 封装对象和参数集合
+            SqlMeta sqlMeta = new SqlMeta();
+            sqlMeta.setTableCode(tableMeta.getCode());
+
+            if (SyncConstant.OPERATE_TYPE_INSERT.equals(tableMeta.getType())) {
+
+
+                // 插入操作
+                InsertBuilder insertBuilder = InsertBuilder.create(dbType);
+                insertBuilder.table(tableMeta.getDest());
+
+                int[] paramTypeList = new int[tableMeta.getColumnMetas().size()];
+
+                int i = 0;
+
+                for (ColumnMeta columnMeta : tableMeta.getColumnMetas()) {
+
+                    insertBuilder.column(columnMeta.getDest());
+                    paramTypeList[i] = JdbcUtils.TYPE_UNKNOWN;
+                    i++;
+                }
+
+                insertBuilder.column("sync_time");
+                List<Object[]> paramList = this.generateParam(resultMetaMap, msgTableMeta, tableMeta, syncTask.getSyncTime(), true);
+
+                insertBuilder.groupCount(paramList.size());
+                sqlMeta.setParamList(paramList);
+                sqlMeta.setSql(insertBuilder.sql());
+                sqlMeta.setArgTypes(paramTypeList);
+                sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_INSERT);
+            }
+
+            if (SyncConstant.OPERATE_TYPE_DELETE.equals(tableMeta.getType())) {
+
+                DeleteBuilder deleteBuilder = DeleteBuilder.create(dbType);
+                deleteBuilder.table(tableMeta.getDest());
+                deleteBuilder.whereIn(tableMeta.getUnique().getValue(), msgTableMeta.getUniqueList().size());
+                deleteBuilder.groupCount(1);
+
+                sqlMeta.setSql(deleteBuilder.sql());
+                sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_DELETE);
+
+                String targetType = msgTableMeta.getUniqueType();
+
+                if ("_id".equals(tableMeta.getUnique().getValue())) {
+                    targetType = SyncConvert.DATA_TYPE_OBJECTID;
+                }
+
+                sqlMeta.setParamList(SyncConvert.toArrayList(SyncConvert.convert(msgTableMeta.getUniqueList(), targetType)));
+            }
+
+            if (SyncConstant.OPERATE_TYPE_UPDATE.equals(tableMeta.getType())) {
+
+                UpdateBuilder updateBuilder = UpdateBuilder.create(dbType);
+                updateBuilder.table(tableMeta.getDest());
+
+                int[] paramTypeList = new int[tableMeta.getColumnMetas().size()];
+                int i = 0;
+
+                for (ColumnMeta columnMeta : tableMeta.getColumnMetas()) {
+
+                    updateBuilder.column(columnMeta.getDest());
+                    paramTypeList[i] = JdbcUtils.TYPE_UNKNOWN;
+                    i++;
+                }
+
+                updateBuilder.column("sync_time");
+                updateBuilder.whereIn(tableMeta.getUnique().getValue(), 1);
+
+                List<Object[]> paramList = this.generateParam(resultMetaMap, msgTableMeta, tableMeta, syncTask.getSyncTime(), false);
+                updateBuilder.groupCount(paramList.size());
+
+                sqlMeta.setParamList(paramList);
+                sqlMeta.setSql(updateBuilder.sql());
+                sqlMeta.setArgTypes(paramTypeList);
+                sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_UPDATE);
+            }
+
+            executor.sql(sqlMeta);
+        }
+
+        // 回调记录
+        if(CollectionUtils.isNotEmpty(syncTask.getPlanMeta().getCallbackMetas())){
+
+            SqlMeta sqlMeta = this.insertCallbackRecord(syncTask, destDb, dbType, syncTask.getPlanMeta().getCallbackMetas());
+
+            executor.sql(sqlMeta);
+        }
+
+    }
+
+    private SqlMeta insertCallbackRecord(SyncTask syncTask, String destDb, String dbType, List<CallbackMeta> callbackMetas) {
+
+        SqlMeta sqlMeta = new SqlMeta();
+        sqlMeta.setTableCode("pub_callback");
+
+
+        // 插入
+        InsertBuilder insertBuilder = InsertBuilder.create(dbType);
+
+        insertBuilder.table("pub_callback");
+        insertBuilder.column("uuid");
+        insertBuilder.column("sync_uuid");
+        insertBuilder.column("src_project");
+        insertBuilder.column("dest_project");
+        insertBuilder.column("db_name");
+        insertBuilder.column("msg_content");
+        insertBuilder.column("create_time");
+        insertBuilder.column("repeat_count");
+        insertBuilder.column("status");
+        insertBuilder.column("remark");
+
+        List<Object[]> paramList = new ArrayList<>();
+        List<CallbackTask> callbackTaskList = new ArrayList<>();
+
+        for (CallbackMeta callbackMeta : callbackMetas) {
+
+
+            List<Object> paramObjectList = new ArrayList<>();
+
+            String uuid = UUIDUtils.getPrimaryValue();
+
+            paramObjectList.add(uuid);
+            paramObjectList.add(syncTask.getResultModel().getUuid());
+            paramObjectList.add(syncTask.getPlanMeta().getProjectMeta().getSrcProject());
+            paramObjectList.add(callbackMeta.getProject());
+            paramObjectList.add(destDb);
+            paramObjectList.add(syncTask.getMsgMeta().getMsg());
+            paramObjectList.add(new Date());
+            paramObjectList.add(0);
+            paramObjectList.add(PlanConstant.CALLBACK_STATUS_WAIT);
+            paramObjectList.add(callbackMeta.getDesc());
+
+            paramList.add(paramObjectList.toArray());
+
+            CallbackTask callbackTask = new CallbackTask();
+
+            callbackTask.setDb(destDb);
+            callbackTask.setFirst(true);
+            callbackTask.setMsg(syncTask.getMsgMeta().getMsg());
+            callbackTask.setUuid(uuid);
+            callbackTask.setProject(callbackMeta.getProject());
+
+            callbackTaskList.add(callbackTask);
+
+        }
+
+        sqlMeta.setParamList(paramList);
+        sqlMeta.setSql(insertBuilder.sql());
+        sqlMeta.setSqlType(SyncConstant.OPERATE_TYPE_INSERT);
+        sqlMeta.setCallbackTaskList(callbackTaskList);
+
+        return sqlMeta;
     }
 
     private List<Object[]> generateParam(Map<String, SqlMeta> resultMetaMap, MsgTableMeta msgTableMeta, TableMeta tableMeta, Date syncTime, boolean insert) {
---
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/thread/CallbackRetryThread.java
@@ -0,0 +1,73 @@
+package com.coderman.sync.thread;
+
+import com.coderman.sync.callback.meta.CallbackTask;
+import com.coderman.sync.context.CallbackContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.DelayQueue;
+
+@Component
+@Lazy(false)
+@Slf4j
+public class CallbackRetryThread {
+
+
+    private final DelayQueue<CallbackTask> queue = new DelayQueue<>();
+
+
+    public void add(CallbackTask callbackTask){
+
+        this.queue.add(callbackTask);
+    }
+
+    @PostConstruct
+    public void init(){
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                while (true){
+
+                    try {
+
+                        CallbackTask callbackTask = queue.take();
+
+                        callbackTask.setFirst(false);
+
+                        CallbackContext.getCallbackContext().addTask(callbackTask);
+
+                    } catch (InterruptedException e) {
+
+                        throw new RuntimeException(e);
+                    }
+
+                }
+
+            }
+
+        });
+
+        thread.setName("回调失败重试线程");
+        thread.setDaemon(true);
+        thread.start();
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+
+                if("回调失败重试线程".equals(t.getName())){
+
+                    init();
+                }
+            }
+        });
+
+        log.info("回调失败重试线程启动...");
+    }
+
+}

4825fff7f88bc0d0d93d54f32c0dd24d9be407da
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-02-05 18:15:16 +0800

    esz整合2

--- base-sync-server/base-sync-service/src/main/java/com/coderman/sync/es/EsService.java
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/es/EsService.java
@@ -1,4 +1,17 @@
 package com.coderman.sync.es;
 
+import com.coderman.sync.result.ResultModel;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface EsService {
+
+    /**
+     * 批量查询同步记录
+     *
+     * @param resultModelList 同步记录list
+     * @return
+     */
+    public boolean batchInsertSyncResult(List<ResultModel> resultModelList) throws IOException;
 }

4b270b5e097745d3de7bcf42d0fcb8773cf03fbe
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-02-20 21:56:14 +0800

    同步系统优化+1

--- base-sync-server/base-sync-service/src/main/java/com/coderman/sync/service/result/ResultService.java
+++ base-sync-server/base-sync-service/src/main/java/com/coderman/sync/service/result/ResultService.java
@@ -1,9 +1,11 @@
 package com.coderman.sync.service.result;
 
 import com.alibaba.fastjson.JSONObject;
+import com.coderman.sync.vo.CompareVO;
 import com.coderman.sync.vo.ResultVO;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface ResultService {
 
@@ -11,8 +13,8 @@ public interface ResultService {
      * 同步记录搜索
      *
      * @param currentPage 当前页
-     * @param pageSize 每页显示条数
-     * @param queryVO 参数条件
+     * @param pageSize    每页显示条数
+     * @param queryVO     参数条件
      * @return
      */
     JSONObject search(Integer currentPage, Integer pageSize, ResultVO queryVO) throws IOException;
@@ -34,4 +36,13 @@ public interface ResultService {
      * @return
      */
     com.coderman.api.vo.ResultVO<Void> signSuccess(String uuid) throws IOException;
+
+
+    /**
+     * 比对同步结果
+     *
+     * @param uuid
+     * @return
+     */
+    com.coderman.api.vo.ResultVO<List<CompareVO>> selectTableData(String uuid, boolean convert) throws Throwable;
 }
--- base-sync-server/base-sync-web/src/main/java/com/coderman/sync/controller/result/ResultController.java
+++ base-sync-server/base-sync-web/src/main/java/com/coderman/sync/controller/result/ResultController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.coderman.swagger.annotation.ApiReturnIgnore;
 import com.coderman.swagger.constant.SwaggerConstant;
 import com.coderman.sync.service.result.ResultService;
+import com.coderman.sync.vo.CompareVO;
 import com.coderman.sync.vo.ResultVO;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/${domain}/result")
@@ -26,8 +28,8 @@ public class ResultController {
     })
     @ApiReturnIgnore
     public JSONObject search(@RequestParam(value = "page", defaultValue = "1") Integer currentPage,
-                           @RequestParam(value = "rows", defaultValue = "20") Integer pageSize, ResultVO resultVO) throws Exception {
-        return this.resultService.search(currentPage,pageSize,resultVO);
+                             @RequestParam(value = "rows", defaultValue = "20") Integer pageSize, ResultVO resultVO) throws Exception {
+        return this.resultService.search(currentPage, pageSize, resultVO);
     }
 
     @GetMapping(value = "/repeat/sync")
@@ -35,7 +37,7 @@ public class ResultController {
             @ApiImplicitParam(name = "uuid", paramType = SwaggerConstant.PARAM_QUERY, dataType = SwaggerConstant.DATA_STRING, value = "uuid", required = true),
     })
     @ApiReturnIgnore
-    public com.coderman.api.vo.ResultVO<Void> repeatSync(String uuid){
+    public com.coderman.api.vo.ResultVO<Void> repeatSync(String uuid) {
 
         return this.resultService.repeatSync(uuid);
     }
@@ -51,7 +53,15 @@ public class ResultController {
     }
 
 
+    @GetMapping(value = "/valid/data")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "uuid", paramType = SwaggerConstant.PARAM_QUERY, dataType = SwaggerConstant.DATA_STRING, value = "uuid", required = true),
+    })
+    @ApiReturnIgnore
+    public com.coderman.api.vo.ResultVO<List<CompareVO>> validResultData(String uuid) throws Throwable {
 
+        return this.resultService.selectTableData(uuid, true);
+    }
 
 
 }

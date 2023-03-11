b1039bc93ad821eb75cb4c20e13d55198f50faf8
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-03-11 12:11:33 +0800

    日志模块整合钉钉告警

---
+++ base-log/src/main/java/com/coderman/log/alarm/AlarmTypeEnum.java
@@ -0,0 +1,26 @@
+package com.coderman.log.alarm;
+
+/**
+ * 告警类型
+ *
+ */
+public enum AlarmTypeEnum {
+    /**
+     * 钉钉
+     */
+    DING_TALK("DingTalk"),
+    /**
+     * 外部系统
+     */
+    EXTERNAL_SYSTEM("ExternalSystem");
+
+    AlarmTypeEnum(String type) {
+        this.type = type;
+    };
+
+    private final String type;
+
+    public String getType() {
+        return type;
+    }
+}

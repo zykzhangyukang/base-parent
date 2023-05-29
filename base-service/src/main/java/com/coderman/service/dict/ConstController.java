71811d6112b5007c638e0a7e734abcc609a5e3c8
zhangyukang <3053161401@qq.com>
2023-05-29 18:34:14 +0800

    项目常量

--- base-service/src/main/java/com/coderman/service/dict/ConstController.java
+++ base-service/src/main/java/com/coderman/service/dict/ConstController.java
@@ -2,12 +2,14 @@ package com.coderman.service.dict;
 
 import com.coderman.api.util.ResultUtil;
 import com.coderman.api.vo.ResultVO;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "项目常量", tags = "项目常量")
 @RestController
 @RequestMapping(value = "/${domain}/const")
 public class ConstController {

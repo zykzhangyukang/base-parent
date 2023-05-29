68f5492fba5d3174b93cd9b6ad9d002e46de07d5
zhangyukang <3053161401@qq.com>
2023-05-29 18:41:38 +0800

    项目常量+1

--- base-service/src/main/java/com/coderman/service/dict/ConstController.java
+++ base-service/src/main/java/com/coderman/service/dict/ConstController.java
@@ -2,7 +2,11 @@ package com.coderman.service.dict;
 
 import com.coderman.api.util.ResultUtil;
 import com.coderman.api.vo.ResultVO;
+import com.coderman.swagger.annotation.ApiReturnParam;
+import com.coderman.swagger.annotation.ApiReturnParams;
+import com.coderman.swagger.constant.SwaggerConstant;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +19,12 @@ import java.util.List;
 public class ConstController {
 
 
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_GET, value = "项目常量列表")
     @GetMapping(value = "/list")
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+            @ApiReturnParam(name = "ConstItems", value = {"group", "itemList"}),
+    })
     public ResultVO<List<ConstItems>> list(){
         return ResultUtil.getSuccessList(ConstItems.class,ConstService.getAllConstList());
     }

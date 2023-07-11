71e8229e9754b992bf984f570fd3cbf48fa7a5f5
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-07-12 00:05:04 +0800

    代码修改

--- base-auth/base-auth-service/src/main/java/com/coderman/auth/dao/dept/DeptDAO.java
+++
@@ -1,8 +0,0 @@
-package com.coderman.auth.dao.dept;
-
-import com.coderman.auth.model.dept.DeptExample;
-import com.coderman.auth.model.dept.DeptModel;
-import com.coderman.mybatis.dao.BaseDAO;
-
-public interface DeptDAO extends BaseDAO<DeptModel, DeptExample> {
-}

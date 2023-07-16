ff36f1102cebb66d4f8ad739ef23f1fcb8bb067b
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-07-16 21:31:09 +0800

    删除跨域配置

--- base-service/src/main/java/com/coderman/service/config/WebMvcConfig.java
+++ base-service/src/main/java/com/coderman/service/config/WebMvcConfig.java
@@ -20,7 +20,6 @@ import java.util.Map;
  */
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
-@SuppressWarnings("all")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
@@ -39,29 +38,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
         filterRegistrationBean.setInitParameters(initParameters);
         return filterRegistrationBean;
     }
-
-    /**
-     * 跨域配置,生产环境谨慎使用
-     *
-     * @return
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        // 允许cookies跨域
-        config.setAllowCredentials(true);
-        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-        config.addAllowedOrigin("*");
-        // #允许访问的头信息,*表示全部
-        config.addAllowedHeader("*");
-        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-        config.setMaxAge(18000L);
-        // 允许提交请求的方法，*表示全部允许
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-
 }

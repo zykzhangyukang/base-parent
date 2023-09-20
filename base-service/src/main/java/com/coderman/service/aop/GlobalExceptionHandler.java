a359db096db138efbbcae44f27bdaf3e97217e7f
zhangyukang <3053161401@qq.com>
2023-09-20 15:59:50 +0800

    状态码调整

--- base-service/src/main/java/com/coderman/service/aop/GlobalExceptionHandler.java
+++ base-service/src/main/java/com/coderman/service/aop/GlobalExceptionHandler.java
@@ -9,10 +9,13 @@ import com.coderman.service.service.BaseService;
 import com.coderman.service.util.IpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,7 +31,7 @@ public class GlobalExceptionHandler extends BaseService {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResultVO<Void> handlerException(HttpServletRequest request, Exception e) {
+    public ResponseEntity<ResultVO<Void>> handlerException(HttpServletRequest request, Exception e) {
 
         ResultVO<Void> resultVO = new ResultVO<>();
 
@@ -39,7 +42,7 @@ public class GlobalExceptionHandler extends BaseService {
 
         } else if (e instanceof RateLimitException) {
 
-            resultVO.setMsg("请求过于频繁");
+            resultVO.setMsg("请求过于频繁！");
             resultVO.setCode(ResultConstant.RESULT_CODE_429);
 
             log.warn("请求过于频繁,ip:{} , url:{}", IpUtil.getIpAddr(), request.getRequestURI());
@@ -47,10 +50,17 @@ public class GlobalExceptionHandler extends BaseService {
 
         } else if (e instanceof HttpRequestMethodNotSupportedException) {
 
-            resultVO.setMsg("请求错误");
+            resultVO.setMsg("请求错误！");
             resultVO.setCode(ResultConstant.RESULT_CODE_400);
 
             log.warn("非法请求:{},url:{}", e.getMessage(), request.getRequestURI());
+
+        }else if(e instanceof NoHandlerFoundException){
+
+            resultVO.setMsg("接口不存在！");
+            resultVO.setCode(ResultConstant.RESULT_CODE_404);
+
+            log.warn("接口不存在:{},url:{}", e.getMessage(), request.getRequestURI());
 
         } else {
 
@@ -60,7 +70,7 @@ public class GlobalExceptionHandler extends BaseService {
             log.error(CommonConstant.GLOBAL_FAIL_MSG + ":{}", request.getRequestURI(), e);
         }
 
-        return resultVO;
+        return new ResponseEntity<>(resultVO , HttpStatus.valueOf(resultVO.getCode()));
     }
 
 

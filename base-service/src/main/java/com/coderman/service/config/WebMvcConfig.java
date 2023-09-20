c8bb76c241f80deee73f5bc82e60f87194252d19
zhangyukang <3053161401@qq.com>
2023-09-20 15:59:50 +0800

    状态码调整

--- base-service/src/main/java/com/coderman/service/config/WebMvcConfig.java
+++ base-service/src/main/java/com/coderman/service/config/WebMvcConfig.java
@@ -5,10 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.HashMap;

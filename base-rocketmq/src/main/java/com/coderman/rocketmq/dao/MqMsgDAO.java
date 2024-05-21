cb923b3385c354d3201127816f0682243f2f60b1
zhangyukang <3053161401@qq.com>
2024-05-21 09:57:37 +0800

    代码优化

--- base-rocketmq/src/main/java/com/coderman/rocketmq/dao/MqMsgDAO.java
+++ base-rocketmq/src/main/java/com/coderman/rocketmq/dao/MqMsgDAO.java
@@ -1,11 +1,51 @@
 package com.coderman.rocketmq.dao;
 
-import com.coderman.mybatis.dao.BaseDAO;
 import com.coderman.rocketmq.model.MqMsgExample;
 import com.coderman.rocketmq.model.MqMsgModel;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author coderman
  */
-public interface MqMsgDAO extends BaseDAO<MqMsgModel, MqMsgExample> {
+public interface MqMsgDAO  {
+
+    /**
+     * 插入
+     *
+     * @param record
+     * @return
+     */
+    int insert(MqMsgModel record);
+
+
+
+    /**
+     * 根据条件查询
+     *
+     * @param example
+     * @return
+     */
+    List<MqMsgModel> selectByExample(MqMsgExample example);
+
+
+    /**
+     * 动态更新
+     *
+     * @param record
+     * @param example
+     * @return
+     */
+    int updateByExampleSelective(@Param("record") MqMsgModel record, @Param("example") MqMsgExample example);
+
+    /**
+     * 根据主键动态更新
+     *
+     * @param record
+     * @return
+     */
+    int updateByPrimaryKeySelective(MqMsgModel record);
+
+
 }

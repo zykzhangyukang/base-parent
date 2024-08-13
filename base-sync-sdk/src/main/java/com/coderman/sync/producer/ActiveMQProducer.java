1485af4bfb589d0eb8eff49123df1ab65021cc22
zhangyukang <3053161401@qq.com>
2024-08-13 11:47:44 +0800

    优化

--- base-sync-sdk/src/main/java/com/coderman/sync/producer/ActiveMQProducer.java
+++ base-sync-sdk/src/main/java/com/coderman/sync/producer/ActiveMQProducer.java
@@ -1,7 +1,6 @@
 package com.coderman.sync.producer;
 
 import com.coderman.service.util.SpringContextUtil;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.apache.activemq.command.ActiveMQTextMessage;
 import org.springframework.jms.core.JmsTemplate;
@@ -14,21 +13,6 @@ import javax.jms.Message;
  */
 @Data
 public class ActiveMQProducer {
-
-    @ApiModelProperty(value = "队列名称")
-    private String queueName;
-
-    @ApiModelProperty(value = "broker地址")
-    private String brokerUrl;
-
-    @ApiModelProperty(value = "用户名")
-    private String username;
-
-    @ApiModelProperty(value = "密码")
-    private String password;
-
-    @ApiModelProperty(value = "是否启用")
-    private boolean enable;
 
     public Message send(String msg) throws JMSException {
         JmsTemplate jmsTemplate = SpringContextUtil.getBean(JmsTemplate.class);

00f6fc57025ce8b25130978fe4f93a025a592c21
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2024-11-17 20:02:49 +0800

    回调验签

--- base-sync-callback/src/main/java/com/coderman/callback/CallbackController.java
+++ base-sync-callback/src/main/java/com/coderman/callback/CallbackController.java
@@ -3,9 +3,13 @@ package com.coderman.callback;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.Feature;
+import com.coderman.api.exception.BusinessException;
 import com.coderman.api.util.ResultUtil;
 import com.coderman.api.vo.ResultVO;
+import com.coderman.service.util.DesUtil;
 import com.coderman.service.util.SpringContextUtil;
+import com.coderman.swagger.annotation.ApiReturnParam;
+import com.coderman.swagger.annotation.ApiReturnParams;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,20 +30,25 @@ import java.util.Optional;
  */
 @RestController
 @RequestMapping(value = "/${domain}/callback")
+@SuppressWarnings("unchecked")
 public class CallbackController {
 
     private static final Logger logger = LoggerFactory.getLogger(CallbackController.class);
 
     @PostMapping(value = "/notify")
-    @SuppressWarnings("unchecked")
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+    })
     public ResultVO<Void> callback(String msg, HttpServletRequest request) {
         try {
 
-            // TODO 回调权限校验
-
-            if (StringUtils.equals(msg, "ping")) {
+            // 回调健康检查
+            if (StringUtils.equalsIgnoreCase(msg, "PING")) {
                 return ResultUtil.getSuccess();
             }
+
+            // 接口验签
+            this.checkSign(request, msg);
 
             // 构建消息参数
             SyncMsg syncMsg = this.parseMsg(msg);
@@ -48,8 +57,7 @@ public class CallbackController {
             CallbackScannerService service = SpringContextUtil.getBean(CallbackScannerService.class);
 
             // 获取回调元信息
-            CallbackMeta callbackMeta = Optional.ofNullable(service.getCallbackMeta(syncMsg.getPlanCode()))
-                    .orElseThrow(() -> new IllegalArgumentException("回调方法未指定,planCode:" + syncMsg.getPlanCode()));
+            CallbackMeta callbackMeta = Optional.ofNullable(service.getCallbackMeta(syncMsg.getPlanCode())).orElseThrow(() -> new IllegalArgumentException("回调方法未指定,planCode:" + syncMsg.getPlanCode()));
 
             // 获取回调方法
             Method method = callbackMeta.getInstantClass().getMethod(callbackMeta.getMethodName(), SyncMsg.class);
@@ -63,9 +71,31 @@ public class CallbackController {
         } catch (IllegalAccessException | InvocationTargetException e) {
             logger.error("回调方法执行失败: ", e);
             return ResultUtil.getFail("回调方法执行失败");
-        } catch (Exception e) {
+        }catch (BusinessException e){
+            logger.error("回调验签异常: ", e);
+            return ResultUtil.getFail(e.getMessage());
+        }catch (Exception e) {
             logger.error("回调处理异常: ", e);
             return ResultUtil.getFail("回调处理异常");
+        }
+    }
+
+    private void checkSign(HttpServletRequest request, String msg) {
+
+        String timestamp = request.getHeader("timestamp");
+        String nonce = request.getHeader("nonce");
+        String sign = request.getHeader("sign");
+
+        if (StringUtils.isBlank(timestamp) || StringUtils.isBlank(nonce) || StringUtils.isBlank(sign)) {
+            throw new BusinessException("【验签失败】非法回调请求！");
+        }
+
+        if (!StringUtils.equals(sign, DesUtil.encrypt((msg + nonce + timestamp), System.getProperty("secret.key")))) {
+            throw new BusinessException("【验签失败】请求签名错误！");
+        }
+
+        if (System.currentTimeMillis() - Long.parseLong(timestamp) > 1000 * 60) {
+            throw new BusinessException("【验签失败】请求已过期！");
         }
     }
 
@@ -88,9 +118,7 @@ public class CallbackController {
         // 使用forEach和lambda简化循环处理
         jsonArray.forEach(item -> {
             JSONObject json = (JSONObject) item;
-            List<String> uniqueList = Optional.ofNullable(json.getJSONArray("unique"))
-                    .map(arr -> arr.toJavaList(String.class))
-                    .orElse(new ArrayList<>());
+            List<String> uniqueList = Optional.ofNullable(json.getJSONArray("unique")).map(arr -> arr.toJavaList(String.class)).orElse(new ArrayList<>());
 
             itemList.add(new SyncMsgItem(json.getString("code"), uniqueList));
         });
